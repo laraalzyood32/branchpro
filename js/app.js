ad1c8992e49b9fee52285a8f4009451891a56fcf
alzyoodlara89 <lara.ma7mood2012@gmail.com>
2021-03-09 13:31:39 +0200

    branch2

--- js/app.js
+++ js/app.js
@@ -1,16 +1,26 @@
 'use strict';
+
+
 function sum(a, b) {
         
     let  Array = [];
     let  sumout = a +b;
     Array[0] = sumout
-    let result = `The sum of ${a} and ${b} is ${sumout}.`
+    let result = `The sum of' + ${a} and ${b} is ${sumout}`;
     Array[1] = result;
     return Array;
     }
 
-    
     testSum(4, 7);
 
 
-    
+function multiply(a, b) {
+    let array =[];
+    let cross=a*b;
+    Array[0] = cross
+    let result1 = `The multiply of ${a} and ${b} is ${cross}.`
+    Array[1] = result1;
+    return Array;
+    }
+    testMultiply(5,9);
+
