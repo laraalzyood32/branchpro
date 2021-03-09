8c5adf6d8db66989b68c8632b755b3f70f992521
alzyoodlara89 <lara.ma7mood2012@gmail.com>
2021-03-09 13:47:44 +0200

    branch3

--- js/app.js
+++ js/app.js
@@ -15,12 +15,29 @@ function sum(a, b) {
 
 
 function multiply(a, b) {
-    let array =[];
+    let Array1 =[];
     let cross=a*b;
-    Array[0] = cross
+    Array1[0] = cross
     let result1 = `The multiply of ${a} and ${b} is ${cross}.`
-    Array[1] = result1;
-    return Array;
+    Array1[1] = result1;
+    return Array1;
     }
     testMultiply(5,9);
 
+    function sumAndMultiply(a, b, c) {
+             let Array2 = []
+        
+          let  add = sum(a, b)[0];
+           let  finalres= sum(add, c)[0];
+           Array2[0] = finalres;
+        
+           let  mul = multiply(a, b)[0];
+          let  finalmul = multiply(mul, c)[0];
+          Array2[1] = finalmul;
+        
+           Array2[2] = `${a} and ${b} and ${c} sum to ${finalres}.`
+          Array2[3] = `The product of ${a} and ${b} and ${c} is ${finalmul}.`
+        return sumAndMultiply;
+        
+         }
+         testSumAndMultiply(4,7,5);
