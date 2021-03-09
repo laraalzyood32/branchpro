631bc0dc95cd0cedd2e2056c951fc3dcdbf8113b
alzyoodlara89 <lara.ma7mood2012@gmail.com>
2021-03-09 09:30:57 +0200

    created project

---
+++ js/app.js
@@ -0,0 +1,14 @@
+function sum(a, b) {
+        
+    let  Array = [];
+    let  sumout = a +b;
+    Array[0] = sumout
+    let result = `The sum of ${a} and ${b} is ${sumout}.`
+    Array[1] = result;
+    return Array;
+    }
+    
+    testSum(4, 7);
+
+
+    
